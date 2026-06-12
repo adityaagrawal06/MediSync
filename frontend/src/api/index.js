bdb959fba973b348fa8fb1d419b1e8e2e27b3d4d
Aditya <aditya@example.com>
2026-06-12 09:05:36 +0000

    feat: Axios base configuration

---
+++ frontend/src/api/index.js
@@ -0,0 +1,10 @@
+export { default as api } from './axios';
+export { default as authAPI } from './auth';
+export { default as patientAPI } from './patient';
+export { default as medicalAPI } from './medical';
+export { default as qrAPI } from './qr';
+
+export * from './auth';
+export * from './patient';
+export * from './medical';
+export * from './qr';
