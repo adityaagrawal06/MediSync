20a72cb887af76c2e31443a1217ac26a556c881e
Aditya <aditya@example.com>
2026-06-15 06:12:48 +0000

    feat: Dashboard & Settings Pages

---
+++ frontend/src/pages/DashboardPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import DashBoard from '../components/DashBoard';
+
+const DashboardPage = () => {
+  return (
+    <Layout>
+      <DashBoard />
+    </Layout>
+  );
+};
+
+export default DashboardPage;
---
+++ frontend/src/pages/SettingsPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import Settings from '../components/Settings'
+
+const SettingsPage = () => {
+  return (
+    <div>
+      <Layout>
+        <Settings/>
+      </Layout>
+    </div>
+  )
+}
+
+export default SettingsPage
